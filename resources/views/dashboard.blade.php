c7f6491ee05b10cb5323efe76fb3980a8a3721e3
Salim66 <salimhasanriad8@gmail.com>
2021-08-10 00:44:30 +0600

    Alpinejs jan-heise

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,14 +1,14 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="font-semibold text-xl text-gray-200 leading-tight">
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
+    <div class="m-10">
+        <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div class="dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 dark:bg-gray-800 dark:text-gray-200 border-gray-200">
                     You're logged in!
                 </div>
             </div>
