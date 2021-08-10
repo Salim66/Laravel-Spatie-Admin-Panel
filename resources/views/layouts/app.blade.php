77d60ffb8eaaffc7d934c5d7d03497df0abac8b6
Salim66 <salimhasanriad8@gmail.com>
2021-08-10 07:51:19 +0600

    User logout functionality create

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,6 +45,10 @@
                                     <a class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark:bg-transparent dark:hover:bg-gray-600 dark:focus:bg-gray-600 dark:focus:text-white dark:hover:text-white dark:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">Link #3</a>
                                 </div>
                             </div>
+                            <form action="{{ route('logout') }}" method="POST">
+                                @csrf
+                                <a class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark:bg-transparent dark:hover:bg-gray-600 dark:focus:bg-gray-600 dark:focus:text-white dark:hover:text-white dark:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="{{ route('logout') }}" onclick="event.preventDefault(); this.closest('form').submit();">Logout</a>
+                            </form>
                         </div>
                     </nav>
             </div>
