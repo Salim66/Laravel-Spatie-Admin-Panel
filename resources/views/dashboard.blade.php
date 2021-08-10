26c45c1221dafefc6e4af926903d4095f8c26e89
Salim66 <salimhasanriad8@gmail.com>
2021-08-10 07:33:53 +0600

    Tailwind table setup , table find https://play.tailwindcsss.com/CrgBmMsvoY

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,13 +5,45 @@
         </h2>
     </x-slot>
 
-    <div class="m-10">
+    <div class="m-6">
+        <div class="w-full mb-2">
+            <a href="" class="m-2 p-2 dark:bg-green-400 rounded dark:text-gray-200">Create Post</a>
+        </div>
         <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div class="py-2 align-middle inline-block min-w-full dark:text-gray-200 sm:px-6 lg:px-8">
+              <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                <table class="min-w-full divide-y divide-gray-200">
+                  <thead class="bg-gray-50 dark:bg-gray-600 dark:text-gray-200">
+                    <tr>
+                      <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-200 uppercase tracking-wider">Id</th>
+                      <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-200 uppercase tracking-wider">Title</th>
+                      <th scope="col" class="relative px-6 py-3">Edit</th>
+                    </tr>
+                  </thead>
+                  <tbody class="bg-white dark:bg-gray-600 dark:text-gray-200 divide-y divide-gray-200">
+                    <tr></tr>
+                    <tr>
+                      <td class="px-6 py-4 whitespace-nowrap">id</td>
+                      <td class="px-6 py-4 whitespace-nowrap">title</td>
+                      <td class="px-6 py-4 text-right text-sm">
+                        <a href="" class="m-2 p-2 dark:bg-green-400 rounded">Edit</a>
+                        <a href="" class="m-2 p-2 dark:bg-green-400 rounded">Publish</a>
+                      </td>
+                    </tr>
+                    <!-- More items... -->
+                  </tbody>
+                </table>
+                <div class="m-2 p-2">Pagination</div>
+              </div>
+            </div>
+          </div>
+
+        {{-- <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div class="dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 dark:bg-gray-800 dark:text-gray-200 border-gray-200">
                     You're logged in!
                 </div>
             </div>
-        </div>
+        </div> --}}
     </div>
 </x-app-layout>
